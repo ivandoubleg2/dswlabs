5dc8df9adbcbb0f7b0474b8662d67361fb6b3147
ivandoubleg <irvinivan96@gmail.com>
2021-02-17 14:21:17 -0600

    as

---
+++ prueba1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>prueba 1 </title>
+</head>
+<body>
+<p>parrafo de texto </p>
+
+ <?php
+    echo "<p>hola mundo!!!</p>";
+    echo "<p>hola mundo!!!</p>";
+
+    for($i=0; $i<0; $i++)
+    	{
+    		echo "<p> hola mundo </p>\n";
+    	}
+?>
+</body>
+
+
+</html>
